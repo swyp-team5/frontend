e66bccbab883b1e896c97987918465d89f197129
Jin <j888777@naver.com>
2026-07-09 16:22:26 +0900

    feat: 앱 시작 시 로그인 상태 복원

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
-import 'package:chack_chack/common/onboarding/OnboardingPage.dart';
-import 'package:chack_chack/employer/home/RHomePage.dart';
+import 'package:chack_chack/common/auth/auth_gate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-
-import 'employee/home/EHomePage.dart';
 
 void main() async {
   // Flutter 바인딩 초기화
@@ -16,26 +12,24 @@ void main() async {
   await initializeDateFormatting('ko_KR', null);
 
   // 카카오 SDK 초기화
-  KakaoSdk.init(
-    nativeAppKey: '05952ada0dceff8e149cd664e5459465',
-  );
+  KakaoSdk.init(nativeAppKey: '05952ada0dceff8e149cd664e5459465');
 
   runApp(
     // Riverpod을 사용하기 위해 ProviderScope로 감싸줍니다.
-    const ProviderScope(
-      child: MyApp(),
-    ),
+    const ProviderScope(child: MyApp()),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Widget? home;
+
+  const MyApp({super.key, this.home});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RHomePage(),
+      home: home ?? const AuthGate(),
     );
   }
 }
