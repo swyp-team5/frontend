eb3581b2c296cacf81fc89a965d660325af68789
AJH4423 <anjunho12@naver.com>
2026-07-19 06:59:50 +0900

    fix: 스케줄 추가했을때 주간 캘린더에 근무자 이름 잘못나오는 부분 수정

--- lib/employer/schedule/models/schedule_model.dart
+++ lib/employer/schedule/models/schedule_model.dart
@@ -1,4 +1,5 @@
 import 'WorkersResponse.dart';
+import 'AssignmentCreateResponse.dart';
 
 class ScheduleModel {
   final String workName;
@@ -8,6 +9,10 @@ class ScheduleModel {
   final List<DateTime> dates;
   final List<WorkerItem> workers;
 
+  /// 서버가 실제로 생성한 근무 파트 정보 (날짜별로 여러 건일 수 있음).
+  /// 재조회(GET) 없이 이 결과를 그대로 기존 목록에 추가하기 위해 보관한다.
+  final List<AssignmentCreateResponse> createdAssignments;
+
   const ScheduleModel({
     required this.workName,
     required this.startTime,
@@ -15,5 +20,6 @@ class ScheduleModel {
     required this.breakTime,
     required this.dates,
     required this.workers,
+    required this.createdAssignments,
   });
 }
